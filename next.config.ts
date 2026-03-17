7d1a29b3a2f484520c77de96b88bae9ee9f838ed
Marcos <marcoschronos@gmail.com>
2026-03-17 10:53:22 +0700

    Update deploy.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/neoPortfolio",
+  assetPrefix: "/neoPortfolio/",
 };
 
 export default nextConfig;
